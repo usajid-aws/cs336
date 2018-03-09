192a73fa681b6f1ce712aeb828d3bf5531c78277
usamasajid123 <usanasm@yahoo.com>
2018-03-09 07:07:29 -0500

    adding two second timer, hoping to fix login and add user issue

--- html/pyflask.py
+++ html/pyflask.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, request,redirect, url_for
-import MySQLdb
-import traceback
+import MySQLdb, traceback, time
 
 app=Flask(__name__)
 
@@ -24,19 +23,19 @@ def signUp():
 	name = str(request.form["name"])
 	username = str(request.form["username"])
 	password = str(request.form["password"])
-	print("ran \n")
 	#database cursor
 	x = conn.cursor()
 	new_user__query = "INSERT INTO Users (Name, Username, Password) Values ('%s', '%s', '%s')" % (name, username, password)
 	try:
 		x.execute(new_user__query)
+		time.sleep(2)
 		conn.commit()
 		return redirect('http://ec2-18-220-2-41.us-east-2.compute.amazonaws.com/')
 	except Exception:
-		print ("\n User wasn't add \n") #User probably already exists, will deal with this later
+		#print ("\n User wasn't add \n") #User probably already exists, will deal with this later
 		traceback.print_exc()
-		print ("\n \n")
-		print(new_user__query)
+		#print ("\n \n")
+		#print(new_user__query)
 		return redirect('http://127.0.0.1:5000/')
 
 @app.route("/login", methods=['POST'])
@@ -45,13 +44,14 @@ def login():
 	index_password = str(request.form["password"])
 	x = conn.cursor()
 	login_query = "Select * from Users where Username='%s'" % (index_username)
-	print (login_query)
+	#print (login_query)
 	x.execute(login_query)
+	time.sleep(2)
 	data_line = x.fetchone()
 	if(data_line is not None and data_line[1] == index_username and data_line[2] == index_password):
 		user = data_line[0]
 		return render_template("welcome.html", user=user)
-	return "<h2> unsuccessful </h2>"
+	return "<h2> unsuccessful login, password dont match or user does not exist!</h2>"
 	
 	
 	
